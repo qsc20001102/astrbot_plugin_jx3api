bdf9e8821d66d8b83f7601980f3e84e385713645
qsc <qsc20001102@163.com>
2025-12-03 13:37:59 +0800

    11

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from .core.jx3_service import JX3Service
 
 
 
-@register("astrbot_plugin_jx3", 
+@register("剑网三数据查询工具", 
           "fxdyz", 
           "通过接口调用剑网三API接口获取游戏数据", 
           "1.0.0",
